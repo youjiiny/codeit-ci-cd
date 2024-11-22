cebba414939aa5a6fe2c1d2d40889d43162aab25
정유진 <jane320@naver.com>
2024-11-22 11:43:05 +0900

    cypress로 Home 페이지 테스트

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000", // 애플리케이션의 로컬 서버 URL
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
